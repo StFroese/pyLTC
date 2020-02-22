65d875d804a4220f23c6a59f2dd0d08b438b8f8a
Stefan Fröse <stefanfroese@web.de>
2020-02-22 19:16:21 +0100

    sine works, .wav works

--- sine_generator.py
+++ sine_generator.py
@@ -1,4 +1,6 @@
 import math
+import itertools
+import numpy as np
 
 class SineGenerator:
 
@@ -12,6 +14,14 @@ class SineGenerator:
         while True:
             yield math.sin(2 * math.pi * self.freq / self.sampleRate * sample)
             sample += 1
+
+    def wave(self, duration=1000): #duration in ms
+        self.duration = duration
+        wave = []
+        for val in itertools.islice(self.generate(), int(self.sampleRate*duration/1000)):
+            wave.append(val)
+        return np.array(wave)    
+
 
         
 
--- test.py
+++ test.py
@@ -1,4 +1,26 @@
 from sine_generator import SineGenerator
+import wave as wv
+import numpy as np
 
-generator = SineGenerator()
-print(generator.generate())
+#96kHz/24Bit testen
+
+generator = SineGenerator(freq=100)
+
+wave = generator.wave(duration=5000)
+wave = (wave * (2**16 - 1)).astype(np.uint16)
+print(wave.dtype)
+print(len(wave))
+wave = wave.tobytes()
+print(len(wave))
+
+
+waveFile = wv.open('test.wav', 'wb')
+waveFile.setnchannels(1)
+waveFile.setsampwidth(2)
+waveFile.setframerate(44100)
+# waveFile.setnframes(44100*5)
+waveFile.writeframes(wave)
+waveFile.close()
+
+
+
